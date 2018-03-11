11c7d67355fcb548b72bb7e4c558c99cfbb71d7b
Fenrir <elvararni@gmail.com>
2018-03-11 17:00:16 +0000

    Lagfæringar - Elvar

--- TripHop/src/triphop/testAPI/TourAPI.java
+++ TripHop/src/triphop/testAPI/TourAPI.java
@@ -10,23 +10,18 @@ package triphop.testAPI;
  * @author Elvar 
  */
 public class TourAPI {
-    public String[][] search() {
-        String[][] data = new String[10][3];
-        
-        String[] loc = new String[10];
-        String[] name = new String[10];
-        String[] price = new String[10];
-        
-        
-        
-        
-        int n = 10;
-        for(int i = 0; i < n; i++) {
-            data[i][0] = loc[i]; 
-            data[i][1] = name[i];
-            data[i][2] = price[i];
-        }
-        
+    public Object[][] search() { 
+        // name, country, city, price
+        Object[][] data = new Object[][] {
+             { "Biking Around Reykjavík", "Iceland", "Reykjavík", 2500 }
+            ,{ "Biking Around Nairobi", "Kenya", "Nairobi", 103 }
+            ,{ "Strolling Around ", "", "", }
+            ,{ "Strolling Around ", "", "", }
+            ,{ "Skiing On Afriski", "Afriski", "Lesotho", }
+            ,{ "Skiing On ", "", "", }
+            ,{ "Golf", "", "", }
+            ,{ "Golf", "", "", }
+        };
         
         return data;
     }     
