d1bbfa0be96e9179ed38bc658c5d89fa4ea58dcb
Ragganovich <Ragganovich@hotmail.com>
2018-04-03 23:07:52 +0000

    pakki inniheldur aðeins eina dagsferð

--- TripHop/src/triphop/control/PackageManager.java
+++ TripHop/src/triphop/control/PackageManager.java
@@ -5,7 +5,7 @@
  */
 package triphop.control;
 
-import triphop.model.Package;
+import triphop.model.*;
 import java.util.*;
 
 /**
@@ -119,47 +119,8 @@ public class PackageManager {
                     // Ath. hvort komið sé út á enda á einhverjum listanum, ef svo er þá hætt.
                     if(flights.get(i) == null || hotels.get(j) == null || dayTours.get(k) == null) break;
                     packageNumber = (i * j) + k;
-                    // Þær dagsferðir sem verða settar í pakkann.
-                    ArrayList<DayTour> packageTours = new ArrayList<DayTour>(numOfDayTours);
-                    if (numOfDayTours != 0) packageTours.add(0, dayTours.get(k));
-                     
-                    // Ef beðið var um fleiri en eina dagsferð eru fundnar aðrar af handahófi.
-                    // Ef beðið er um fleiri dagsferðir en leit hefur fundið er aðeins gefin ein dagsferð.
-                    if(numOfDayTours > 1 && numOfDayTours < dayTours.size()) {
-                        int[] selectedTours = new int[numOfDayTours];
-                        selectedTours[0] = k;
-                        // Byrja á að setja stök fylkisins sem -1, svo hægt sé að gá
-                        // hvort búið sé að setja inn í það seinna
-                        for(int o = 1; o < selectedTours.length; o++) {
-                            selectedTours[o] = -1;
-                        }
-                        for(int p = 1; p < numOfDayTours; p++) {
-                            // Dagsferð valin af handahófi til að hafa með hinum dagsferðum þessa pakka.
-                            int randomTour; 
-                            randomTour = (int) Math.floor(Math.random() * dayTours.size());
-                            int x = 0;
-                            // Gáð hvort sama dagsferð hafi verið valin áður
-                            while(x < numOfDayTours) {
-                                // Ef ekkert hefur verið sett inn enn
-                                if(selectedTours[x] == -1) break;
-                                    // Ef hitt er óvart ferð sem var valin áður er ný valin af handahófi
-                                    // og byrjað upp á nýtt að bera saman.
-                                if(randomTour == selectedTours[x]) {
-                                    randomTour = (int) Math.floor(Math.random() * dayTours.size());
-                                    x = 0;
-                                }
-                                // Ef tölurnar eru ekki eins eru þær næstu bornar saman.
-                                else x++;
-                            }
-                            // Valinn túr merktur inn svo hann verði ekki valinn aftur.
-                            selectedTours[p] = randomTour;
-                            // Völdum túr bætt inn.
-                            packageTours.add(p, dayTours.get(randomTour));
-                        }
-                    }
-                     
                     // Nýr pakki búinn til og honum bætt í listann.
-                    Package pack = new Package(flights.get(i), hotels.get(j), packageTours);
+                    Package pack = new Package(flights.get(i), hotels.get(j), dayTours.get(k));
                     packages.add(packageNumber, pack);
                 }
             }
